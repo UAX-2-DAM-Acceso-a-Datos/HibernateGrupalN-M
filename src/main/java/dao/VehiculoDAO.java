aa7cb4f9683380c50a9220032fb4f6019d61a19e
CTG099 <carlostorresgragera@gmail.com>
2022-11-15 23:17:32 +0100

    Deletevehiculos

--- src/main/java/dao/VehiculoDAO.java
+++ src/main/java/dao/VehiculoDAO.java
@@ -1,9 +1,16 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.hibernate.Session;
+
 import dto.VehiculoDTO;
+import entities.Cuenta;
 import idao.IVehiculo;
+import utils.HibernateUtils;
 
 public class VehiculoDAO implements IVehiculo {
 
@@ -21,7 +28,18 @@ public class VehiculoDAO implements IVehiculo {
 
 	@Override
 	public boolean deleteVehiculo(VehiculoDTO v) {
+		
+		//Habria que introducir un valor int ya que sino no se podria eliminar por id
+		
+		
 		// TODO Auto-generated method stub
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		session.createQuery("DELETE FROM vehiculos WHERE" + v.getMatricula());
+		
+		session.beginTransaction();
+		
+		System.out.println("El vehiculo ha sido eliminado");
+		session.close();
 		return false;
 	}
 
