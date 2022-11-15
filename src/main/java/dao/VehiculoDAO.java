2d6da09dda9f7c143972df601a6423d706725460
LAPTOP-RAFA <rafaeldijkstra@gmail.com>
2022-11-15 09:54:18 +0100

    Interfaz vehiculo e implements

--- src/main/java/dao/VehiculoDAO.java
+++ src/main/java/dao/VehiculoDAO.java
@@ -1,5 +1,35 @@
 package dao;
 
-public class VehiculoDAO {
+import java.util.List;
+
+import dto.VehiculoDTO;
+import idao.IVehiculo;
+
+public class VehiculoDAO implements IVehiculo {
+
+	@Override
+	public boolean addVehiculo(VehiculoDTO v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateVehiculo(VehiculoDTO v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteVehiculo(VehiculoDTO v) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<VehiculoDTO> listVehiculo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
--- src/main/java/idao/IVehiculo.java
+++ src/main/java/idao/IVehiculo.java
@@ -1,5 +1,12 @@
 package idao;
 
-public interface IVehiculo {
+import java.util.List;
 
+import dto.VehiculoDTO;
+
+public interface IVehiculo {
+	public boolean addVehiculo(VehiculoDTO v);
+	public boolean updateVehiculo(VehiculoDTO v);
+	public boolean deleteVehiculo(VehiculoDTO v);
+	public List<VehiculoDTO> listVehiculo();
 }
