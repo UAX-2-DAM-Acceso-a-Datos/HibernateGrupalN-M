5d41a90d2443a09b68c6deb374b90b4f82546719
LAPTOP-RAFA <rafaeldijkstra@gmail.com>
2022-11-15 10:22:52 +0100

    metodo updateVehiculo

--- src/main/java/dao/VehiculoDAO.java
+++ src/main/java/dao/VehiculoDAO.java
@@ -2,8 +2,11 @@ package dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
 import dto.VehiculoDTO;
 import idao.IVehiculo;
+import utils.HibernateUtils;
 
 public class VehiculoDAO implements IVehiculo {
 
@@ -15,8 +18,13 @@ public class VehiculoDAO implements IVehiculo {
 
 	@Override
 	public boolean updateVehiculo(VehiculoDTO v) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		Session session = HibernateUtils.getSessionFactory().openSession();// Abrir sesion
+		session.update(v); // Update Vehiculo
+		session.beginTransaction().commit();// commit del Update 
+		session.close();
+		
+		return true;
 	}
 
 	@Override
