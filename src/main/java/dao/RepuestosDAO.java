c6c42686f762b341e8e6db84114c936be5002b9d
LAPTOP-RAFA <rafaeldijkstra@gmail.com>
2022-11-16 09:14:02 +0100

    updateRepuestos

--- src/main/java/dao/RepuestosDAO.java
+++ src/main/java/dao/RepuestosDAO.java
@@ -2,15 +2,22 @@ package dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
 import dto.RepuestosDTO;
 import idao.IRepuestos;
+import utils.HibernateUtils;
 
 public class RepuestosDAO implements IRepuestos {
 
 	@Override
 	public boolean updateRepuestos(RepuestosDTO r) {
-		// TODO Auto-generated method stub
-		return false;
+		Session session = HibernateUtils.getSessionFactory().openSession();// Abrir sesion
+		session.update(r); // Update Vehiculo
+		session.beginTransaction().commit();// commit del Update 
+		session.close();
+		
+		return true;
 	}
 
 	@Override
