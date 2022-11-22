a506b060d0389098cf5110518f9b284ce38b5b52
Hectorblanco <102791559+Hectorblanco06@users.noreply.github.com>
2022-11-22 10:14:01 +0100

    Creacion del paquete de Pruebas JUnit

---
+++ src/test/java/JUnitTest/TestRepuestos.java
@@ -0,0 +1,14 @@
+package JUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRepuestos {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
