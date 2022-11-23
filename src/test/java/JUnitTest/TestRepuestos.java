327854978a8d32d8e16608d4d28f4eea3632207f
LAPTOP-RAFA <rafaeldijkstra@gmail.com>
2022-11-23 10:45:37 +0100

    Test updateVehiculo, Test updateRepuestos

--- src/test/java/JUnitTest/TestRepuestos.java
+++ src/test/java/JUnitTest/TestRepuestos.java
@@ -1,14 +1,22 @@
 package JUnitTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import dao.RepuestosDAO;
+import dao.VehiculoDAO;
+import pojo.RepuestosPOJO;
+import pojo.VehiculoPOJO;
 
 class TestRepuestos {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void updateRepuesto() {
+		RepuestosPOJO rPOJO = new RepuestosPOJO(3,"Repuestos2","RepuestosPrueba");
+		RepuestosDAO rDAO = new RepuestosDAO();
+		
+		
+		Assert.assertTrue(rDAO.modificarRepuestos(rPOJO));
+		
 	}
-
 }
