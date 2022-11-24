e1541146931c445c7e321f6732c2e7dd59f08a72
Hectorblanco <102791559+Hectorblanco06@users.noreply.github.com>
2022-11-24 12:15:50 +0100

    Cambio por trastorno del rafa

--- src/test/java/JUnitTest/TestVehiculo.java
+++ src/test/java/JUnitTest/TestVehiculo.java
@@ -12,27 +12,21 @@ import pojo.VehiculoPOJO;
 class TestVehiculo {
 
 	@Test
-
 	public void updateVehiculo() {
 		VehiculoPOJO vPOJO = new VehiculoPOJO("2324JZZ","Peugeot","206");
 		VehiculoDAO vDAO = new VehiculoDAO();
-		
-		
 		Assert.assertTrue(vDAO.updateVehiculo(vPOJO));
 	}
-		
+		@Test
 		public void addVehiculo() {
-
-		
 			VehiculoDAO v1 = new VehiculoDAO();
 			VehiculoPOJO vPojo = new VehiculoPOJO("1505FRT", "PEUGOT", "X3");
 			boolean result = v1.addVehiculo(vPojo);
-		
 			Assert.assertTrue(result); 
 			
 		}
 
 		}
 
-	}
+	
 
