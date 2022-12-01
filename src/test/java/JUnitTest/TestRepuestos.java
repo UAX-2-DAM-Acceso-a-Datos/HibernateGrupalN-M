a3286ec1fd4d02c9dadb7458dc77cd55a4f3938a
sergioromerohd <sergio.romero.hombre@gmail.com>
2022-12-01 12:14:39 +0100

    arreglo para test

--- src/test/java/JUnitTest/TestRepuestos.java
+++ src/test/java/JUnitTest/TestRepuestos.java
@@ -1,6 +1,10 @@
 package JUnitTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -31,5 +35,16 @@ class TestRepuestos {
 		assertTrue(r.eliminarRepuestos(r1));
 		
 		
+	}
+	@Test
+	void listrepuestos() {
+		RepuestosDAO redao=new RepuestosDAO();
+		List<RepuestosPOJO> a=new ArrayList<>();
+		VehiculoPOJO vehpoj=new VehiculoPOJO("123456","306","Peugeot");
+		List<VehiculoPOJO>av=new ArrayList<>();
+		av.add(vehpoj);
+		RepuestosPOJO b=new RepuestosPOJO(1,"Repuesto", "Repuestoprueba",av);
+		a.add(b);
+		assertEquals(a, redao.listarRespuestos());
 	}
 }
--- src/test/java/JUnitTest/TestVehiculo.java
+++ src/test/java/JUnitTest/TestVehiculo.java
@@ -1,6 +1,10 @@
 package JUnitTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -8,6 +12,7 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import dao.VehiculoDAO;
+import pojo.RepuestosPOJO;
 import pojo.VehiculoPOJO;
 
 class TestVehiculo {
@@ -27,6 +32,18 @@ class TestVehiculo {
 		boolean result = v1.addVehiculo(vPojo);
 		Assert.assertTrue(result);
 
+	}
+	@Test
+	void listvehi() {
+		VehiculoDAO redao=new VehiculoDAO();
+		List<VehiculoPOJO> a=new ArrayList<>();
+		RepuestosPOJO b=new RepuestosPOJO(1,"Repuesto", "Repuestoprueba");
+		List<RepuestosPOJO>ar=new ArrayList<>();
+		ar.add(b);
+		VehiculoPOJO vehpoj=new VehiculoPOJO("123456","Peugeot","306",ar);
+		
+		a.add(vehpoj);
+		assertEquals (a, redao.listVehiculo());
 	}
 
 	@Test
